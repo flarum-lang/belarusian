af06eb3d2100a08fdeca14c81220554fd8520b61
Robert Korulczyk <robert@korulczyk.pl>
2023-05-14 15:31:56 +0200

    Update repository template.

--- locale/config.js
+++ locale/config.js
@@ -1,6 +1,6 @@
-// source: https://unpkg.com/dayjs@v1.10.4/locale/xx.js
-// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/xx.js
+// source: https://unpkg.com/dayjs@v1.10.4/locale/be.js
+// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/be.js
 
-// TODO: copy compiled translations from https://unpkg.com/dayjs@v1.10.4/locale/xx.js
+!function(_,e){"object"==typeof exports&&"undefined"!=typeof module?module.exports=e(require("dayjs")):"function"==typeof define&&define.amd?define(["dayjs"],e):_.dayjs_locale_be=e(_.dayjs)}(this,function(_){"use strict";_=_&&_.hasOwnProperty("default")?_.default:_;var e={name:"be",weekdays:"нядзелю_панядзелак_аўторак_сераду_чацвер_пятніцу_суботу".split("_"),months:"студзеня_лютага_сакавіка_красавіка_траўня_чэрвеня_ліпеня_жніўня_верасня_кастрычніка_лістапада_снежня".split("_"),weekStart:1,weekdaysShort:"нд_пн_ат_ср_чц_пт_сб".split("_"),monthsShort:"студ_лют_сак_крас_трав_чэрв_ліп_жнів_вер_каст_ліст_снеж".split("_"),weekdaysMin:"нд_пн_ат_ср_чц_пт_сб".split("_"),ordinal:function(_){return _},formats:{LT:"HH:mm",LTS:"HH:mm:ss",L:"DD.MM.YYYY",LL:"D MMMM YYYY г.",LLL:"D MMMM YYYY г., HH:mm",LLLL:"dddd, D MMMM YYYY г., HH:mm"}};return _.locale(e,null,!0),e});
 
-dayjs.locale('xx');
+dayjs.locale('be');
